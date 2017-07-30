9e681d117058d1f54f488beabcc45d01abde060e
Administrator <Administrator@AFAAW-704030721>
2017-07-30 13:33:26 +0800

    添加项目文件。

---
+++ SoulShop/Areas/Travels/TravelsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SoulShop.Areas.Travels
+{
+    public class TravelsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Travels";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Travels_default",
+                "Travels/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
